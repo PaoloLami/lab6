b4538eff79f818aa40139e40fbb4e75d70305ffc
Paolo Lami <paololami2000@gmail.com>
2021-11-14 23:57:41 +0000

    er

--- lab6.py
+++ lab6.py
@@ -1,13 +1,13 @@
 import time
 import RPi.GPIO as GPIO
-import LED8x8 as LED
+import LED8x8
 
 
 dataPin, latchPin, clockPin = 23, 24, 25
+disp = LED8x8(dataPin, latchPin, clockPin)
 
 try: 
   while True:
-    disp = LED(dataPin, latchPin, clockPin)
     for n in range(8):
       disp.display(n)
       time.sleep(0.001)
