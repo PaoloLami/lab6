0e2a7d59c242e4daf6cde486b54b47ca0ef1cf4c
Paolo Lami <paololami2000@gmail.com>
2021-11-15 04:31:50 +0000

    test

--- LED8x8.py
+++ LED8x8.py
@@ -25,8 +25,7 @@ dataPin, latchPin, clockPin = 23, 24, 25
 disp = LED8x8(dataPin, latchPin, clockPin)
 
 try:
-  while True:
-    pass
+  LED8x8.p.start()
 except KeyboardInterrupt:
   GPIO.cleanup()
   disp.p.terminate() 
