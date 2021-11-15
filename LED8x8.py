f322bdc0e93117c304e5d1a48f2ada51c9049185
Paolo Lami <paololami2000@gmail.com>
2021-11-15 05:52:07 +0000

    test

--- LED8x8.py
+++ LED8x8.py
@@ -3,8 +3,20 @@ import time
 import multiprocessing
 import random
 
+pattern = [
+0b10000000, # 0
+0b01000000, # 1
+0b00100000, # 2
+0b00010000, # 3
+0b00001000, # 4
+0b00000100, # 5
+0b00000010, # 6
+0b00000001, # 7
+]
+
 
 class LED8x8():
+
 
   def __init__(self, data, latch, clock, row,col):
     self.shifter = Shifter(data, latch, clock)
@@ -14,27 +26,26 @@ class LED8x8():
 
   def display(self, row, col):
     #pat[0], pat[1], pat[2], pat[3], pat[4], pat[5], pat[6], pat[7] = 0b11000011, 0b10111101, 0b01011010, 0b01111110, 0b01011010, 0b01100110, 0b10111101, 0b11000011
-    pattern = col
     while True:
-      self.shifter.shiftByte(pattern)  # load the row values
+      self.shifter.shiftByte(pattern[col])  # load the row values
       self.shifter.shiftByte(1 << (row-1)) #select current row
       time.sleep(0.001)
 
   def bug(self, row, col):
-    while True:
-      x = random.randint(-1, 1)
-      y = random.randint(-1, 1)
-      row += y
-      if x == -1:
-        col << 1
-      elif x == 1:
-        col >> 1
-      if row < 0: 
-        row = 0
-      if row > 7:
-        row = 7
-      return row, col
-      time.sleep(0.1)
+    x = random.randint(-1, 1)
+    y = random.randint(-1, 1)
+    row += y
+    col += x
+    if col < 0: 
+      col = 0
+    if col > 7:
+      col = 7
+    if row < 0: 
+      row = 0
+    if row > 7:
+      row = 7
+    return row, col
+    
 
 
 
--- lab6.py
+++ lab6.py
@@ -1,17 +1,20 @@
 import RPi.GPIO as GPIO
 from LED8x8 import LED8x8
 import multiprocessing
+import time
 
 col = multiprocessing.Value('i')
 col.value = 0b00010000
-row = 4
+row = multiprocessing.Value('i')
+row.value = 4
 
 dataPin, latchPin, clockPin = 23, 24, 25
 disp = LED8x8(dataPin, latchPin, clockPin, row, col)
 
 try:
   while True:
-    disp.bug(row,col)
+    row.value,col.value = disp.bug(row,col)
+    time.sleep(0.1)
 except KeyboardInterrupt:
   GPIO.cleanup()
   disp.p.terminate() 
