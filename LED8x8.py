46b4865a2ee8b8cdf634aa634975f395a0577fd2
Paolo Lami <paololami2000@gmail.com>
2021-11-15 04:38:04 +0000

    test

--- LED8x8.py
+++ LED8x8.py
@@ -10,8 +10,8 @@ class LED8x8():
 
   def __init__(self, data, latch, clock):
     self.shifter = Shifter(data, latch, clock)
-    p = multiprocessing.Process(target=self.display)
-    p.daemon = True
+    #p = multiprocessing.Process(target=self.display)
+    #p.daemon = True
     #p.start()
 
   def display(self):
