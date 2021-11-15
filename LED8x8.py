d4d4b89f49b4b95a1cc8d6a6161865fa8d30aa58
Paolo Lami <paololami2000@gmail.com>
2021-11-15 04:41:28 +0000

    test

--- LED8x8.py
+++ LED8x8.py
@@ -3,21 +3,21 @@ import time
 import RPi.GPIO as GPIO
 import multiprocessing
 
-pattern = [0b11000011, 0b10111101, 0b01011010, 0b01111110, 0b01011010, 0b01100110, 0b10111101, 0b11000011]#multiprocessing.Array('i', 8)
-#pat[0], pat[1], pat[2], pat[3], pat[4], pat[5], pat[6], pat[7] = 0b11000011, 0b10111101, 0b01011010, #0b01111110, 0b01011010, 0b01100110, 0b10111101, 0b11000011
+pat = multiprocessing.Array('i', 8)
 
 class LED8x8():
 
   def __init__(self, data, latch, clock):
     self.shifter = Shifter(data, latch, clock)
-    #p = multiprocessing.Process(target=self.display)
-    #p.daemon = True
-    #p.start()
+    p = multiprocessing.Process(target=self.display)
+    p.daemon = True
+    p.start()
 
   def display(self):
+    pat[0], pat[1], pat[2], pat[3], pat[4], pat[5], pat[6], pat[7] = 0b11000011, 0b10111101, 0b01011010, 0b01111110, 0b01011010, 0b01100110, 0b10111101, 0b11000011
     while True:
-      for n in range(len(pattern)):
-        self.shifter.shiftByte(pattern[n])  # load the row values
+      for n in range(8):
+        self.shifter.shiftByte(pat[n])  # load the row values
         self.shifter.shiftByte(1 << (n)) #select current row
         time.sleep(0.001)
 
@@ -25,7 +25,8 @@ dataPin, latchPin, clockPin = 23, 24, 25
 disp = LED8x8(dataPin, latchPin, clockPin)
 
 try:
-  disp.display()
+  while True:
+    pass
 except KeyboardInterrupt:
   GPIO.cleanup()
   disp.p.terminate() 
