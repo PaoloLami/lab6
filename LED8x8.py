d9cbab563025842a1dbe2925507297567128c80f
Paolo Lami <paololami2000@gmail.com>
2021-11-15 03:50:17 +0000

    test

--- LED8x8.py
+++ LED8x8.py
@@ -3,7 +3,8 @@ import time
 import RPi.GPIO as GPIO
 import multiprocessing
 
-pattern = [0b11000011, 0b10111101, 0b01011010, 0b01111110, 0b01011010, 0b01100110, 0b10111101, 0b11000011]
+pattern = multiprocessing.Array('i', 8)
+pattern[0], pattern[1], pattern[2], pattern[3], pattern[4], pattern[5], pattern[6], pattern[7] = 0b11000011, 0b10111101, 0b01011010, 0b01111110, 0b01011010, 0b01100110, 0b10111101, 0b11000011
 
 class LED8x8():
 
@@ -17,7 +18,6 @@ class LED8x8():
         self.shifter.shiftByte(1 << (n)) #select current row
         time.sleep(0.001)
     
-
   p = multiprocessing.Process(target=display,args=(pattern))
 
 dataPin, latchPin, clockPin = 23, 24, 25
