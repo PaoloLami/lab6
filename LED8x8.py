32f994d5da9b1e2bcb017917564554b9df5be922
Paolo Lami <paololami2000@gmail.com>
2021-11-15 04:15:25 +0000

    test

--- LED8x8.py
+++ LED8x8.py
@@ -4,20 +4,21 @@ import RPi.GPIO as GPIO
 import multiprocessing
 
 pattern = multiprocessing.Array('i', 8)
-pattern[0], pattern[1], pattern[2], pattern[3], pattern[4], pattern[5], pattern[6], pattern[7] = 0b11000011, 0b10111101, 0b01011010, 0b01111110, 0b01011010, 0b01100110, 0b10111101, 0b11000011
+
 
 class LED8x8():
 
   def __init__(self, data, latch, clock):
     self.shifter = Shifter(data, latch, clock)
-    p = multiprocessing.Process(target=LED8x8.display())
+    p = multiprocessing.Process(target=LED8x8.display,args=(pattern,))
     p.daemon = True
     p.start()
 
-  def display(self):
+  def display(self,pat):
+    pat[0], pat[1], pat[2], pat[3], pat[4], pat[5], pat[6], pat[7] = 0b11000011, 0b10111101, 0b01011010, 0b01111110, 0b01011010, 0b01100110, 0b10111101, 0b11000011
     while True:
       for n in range(8):
-        self.shifter.shiftByte(pattern[n])  # load the row values
+        self.shifter.shiftByte(pat[n])  # load the row values
         self.shifter.shiftByte(1 << (n)) #select current row
         time.sleep(0.001)
 
