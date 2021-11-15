194c65475669b5db0cda5828b313f43669f39415
Paolo Lami <paololami2000@gmail.com>
2021-11-15 05:56:09 +0000

    test

--- LED8x8.py
+++ LED8x8.py
@@ -27,8 +27,8 @@ class LED8x8():
   def display(self, row, col):
     #pat[0], pat[1], pat[2], pat[3], pat[4], pat[5], pat[6], pat[7] = 0b11000011, 0b10111101, 0b01011010, 0b01111110, 0b01011010, 0b01100110, 0b10111101, 0b11000011
     while True:
-      self.shifter.shiftByte(pattern[col])  # load the row values
-      self.shifter.shiftByte(1 << (row-1)) #select current row
+      self.shifter.shiftByte(pattern[col.value])  # load the row values
+      self.shifter.shiftByte(1 << (row.value-1)) #select current row
       time.sleep(0.001)
 
   def bug(self, row, col):
