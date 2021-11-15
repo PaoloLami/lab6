4d19f50948be66afb0507031e82cd4b4f019b235
Paolo Lami <paololami2000@gmail.com>
2021-11-15 03:36:26 +0000

    multi proc

--- LED8x8.py
+++ LED8x8.py
@@ -1,17 +1,36 @@
 from shifter import Shifter 
+import time
+import RPi.GPIO as GPIO
+import multiprocessing
 
-pattern1 = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
 pattern = [0b11000011, 0b10111101, 0b01011010, 0b01111110, 0b01011010, 0b01100110, 0b10111101, 0b11000011]
+myArray = multiprocessing.Array('i',8)
 
 class LED8x8():
 
   def __init__(self, data, latch, clock):
     self.shifter = Shifter(data, latch, clock)
- 
-  def display(self, num):
-    self.shifter.shiftByte(pattern[num])  # load the row values
-    self.shifter.shiftByte(1 << (num)) #select current row
-  
+
+  def display(self):
+    while True:
+      for n in range(8):
+        self.shifter.shiftByte(pattern[n])  # load the row values
+        self.shifter.shiftByte(1 << (n)) #select current row
+        time.sleep(0.001)
+    
+
+  p = multiprocessing.Process(name='display',target=display,args=(pattern,myArray))
+  p.daemon = True
+  p.start() 
+
+dataPin, latchPin, clockPin = 23, 24, 25
+disp = LED8x8(dataPin, latchPin, clockPin)
+try:
+  disp.display()
+except KeyboardInterrupt:
+  GPIO.cleanup()
+  disp.p.terminate() 
+  disp.p.join(2) 
 
 
 
